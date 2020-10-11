101e51eadf77c955fe3ba03ee81f39522393e196
Acun915 <marciniec.przemyslaw33@gmail.com>
2020-10-11 10:49:33 +0200

    HomeController.java - created getHome() method, on localhost8080, I can see "Hello World"

--- src/main/java/com/przemyslawmarciniec/module/home/HomeController.java
+++ src/main/java/com/przemyslawmarciniec/module/home/HomeController.java
@@ -1,4 +1,13 @@
 package com.przemyslawmarciniec.module.home;
 
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
 public class HomeController {
+    @RequestMapping("/")
+    public String getHome() {
+        return "home";
+    }
 }
